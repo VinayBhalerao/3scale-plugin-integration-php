ba510e47994640d07e1c364a69a4ccecff592448
vinay3scale <vinay@3scale.net>
2015-09-19 05:18:54 +0530

    make it better

--- init/index.php
+++ init/index.php
@@ -1,30 +1,4 @@
 <?php
-// This is the API, 2 possibilities: show the app list or show a specific app by id.
-// This would normally be pulled from a database but for demo purposes, I will be hardcoding the return values.
-
-function get_app_by_id($id)
-{
-  $app_info = array();
-
-  // normally this info would be pulled from a database.
-  // build JSON array.
-  switch ($id){
-    case 1:
-      $app_info = array("app_name" => "Web Demo", "app_price" => "Free", "app_version" => "2.0"); 
-      break;
-    case 2:
-      $app_info = array("app_name" => "Audio Countdown", "app_price" => "Free", "app_version" => "1.1");
-      break;
-    case 3:
-      $app_info = array("app_name" => "The Tab Key", "app_price" => "Free", "app_version" => "1.2");
-      break;
-    case 4:
-      $app_info = array("app_name" => "Music Sleep Timer", "app_price" => "Free", "app_version" => "1.9");
-      break;
-  }
-
-  return $app_info;
-}
 
 function get_app_list()
 {
@@ -35,7 +9,7 @@ function get_app_list()
   return $app_list;
 }
 
-$possible_url = array("get_app_list", "get_app");
+$possible_url = array("get_app_list");
 
 $value = "An error has occurred";
 
@@ -45,12 +19,6 @@ if (isset($_GET["action"]) && in_array($_GET["action"], $possible_url))
     {
       case "get_app_list":
         $value = get_app_list();
-        break;
-      case "get_app":
-        if (isset($_GET["id"]))
-          $value = get_app_by_id($_GET["id"]);
-        else
-          $value = "Missing argument";
         break;
     }
 }
