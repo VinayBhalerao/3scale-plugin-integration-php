20e4f832270b3b293184c86f409d1e33bcb6c7ba
vinay3scale <vinay@3scale.net>
2015-09-22 07:22:15 +0530

    changed path

--- init/index.php
+++ init/index.php
@@ -1,6 +1,5 @@
 <?php
 
-ini_set('include_path', '/home/github_repo/php-plugin/sample-php/init/3scale_ws_api_for_php/');
 require_once('lib/ThreeScaleClient.php');
 
 $client =  new ThreeScaleClient("9062f7afd5a594f2e9882c34206eea67");
