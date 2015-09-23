99e1ec4283ed751920ad5c660b4aa5c09616dbb1
vinay3scale <vinay@3scale.net>
2015-09-24 05:29:17 +0530

    remove provider key

--- init/index.php
+++ init/index.php
@@ -2,7 +2,7 @@
 
 require_once('lib/ThreeScaleClient.php');
 
-$client =  new ThreeScaleClient("ecbffa8e236aba08f98c50075d207f4c");
+$client =  new ThreeScaleClient("YOUR_PROVIDER_KEY");
 
 
 $response = $client->authrep_with_user_key($_GET["user_key"], array('hits' => 1));
